5dad0db6419c2447d109a71f7592cc65c97d0571
StognienkoVladislav <stognienkovv@gmail.com>
2019-03-29 12:04:31 +0200

    connected to local hive (need hive-site.xml in spark conf)

--- PySparkIntro/hive/script.py
+++ PySparkIntro/hive/script.py
@@ -6,15 +6,27 @@ from pyspark.sql import SparkSession, HiveContext
 
 sc = SparkContext("local", "Spark Hive App")
 
-spark = SparkSession.builder.enableHiveSupport().getOrCreate()
+spark = SparkSession.builder.\
+    master('local').\
+    config('spark.sql.warehouse.dir', '/user/hive/warehouse').\
+    config('hive.metastore.uris', 'thrift://localhost:9083').\
+    enableHiveSupport().\
+    getOrCreate()
 
 # spark.sql("CREATE TABLE IF NOT EXISTS src (key INT, value STRING) USING hive")
 
 # spark.sql("LOAD DATA LOCAL INPATH '../data/kv' INTO TABLE src")
 
 # spark.sql("SELECT * FROM src;").show()
+# hive_context = HiveContext(sc)
+
+# check = hive_context.table('test_db.hive_test')
+# check.show()
+
 spark.sql("show databases").show()
-spark.sql("select * from src").show()
+spark.sql('select * from test_db.hive_test').show()
+
+# spark.sql("select * from src").show()
 # spark.sql("select * from default.employee").show()
 # spark.sql("select * from employee limit 1").show()
 
